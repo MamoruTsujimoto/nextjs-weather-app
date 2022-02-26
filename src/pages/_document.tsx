63294037fdaa28cdb41163544ae11d9d3727544c
TsujimotoSirChristopherMamoru <tsujimotosirchristophermamoru@gmail.com>
2022-02-26 10:44:44 +0900

    add: OGP Image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,20 +6,21 @@ export default class Document extends NextDocument {
     return (
       <Html lang='ja'>
         <Head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#'>
-          <meta name="robots" content="noindex" />
+          <meta name='robots' content='noindex' />
           <meta name='theme-color' content='#ffffff' />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" href="/android-chrome-192x192.png" />
+          <link rel='shortcut icon' href='/favicon.ico' />
+          <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' href='/android-chrome-192x192.png' />
           <meta property='og:type' content='website' />
           <meta property='og:url' content='https://nextjs-weather-app-chi.vercel.app/' />
           <meta property='og:title' content='NextJS Weather APP' />
           <meta property='og:description' content='Experimental site using Nextjs and OpenWeather API' />
+          <meta property='og:image' content='/nextjs-weather-app.jpg' />
           <meta name='twitter:card' content='/nextjs-weather-app.jpg' />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-          <meta name="theme-color" content="#718096" />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
+          <meta name='theme-color' content='#718096' />
         </Head>
         <body>
           <Main />
